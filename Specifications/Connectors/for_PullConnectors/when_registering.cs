396b1d91bb58cde920a7c1d31f5a6a2010d42dae
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-09 19:38:39 +0200

    Adding specs for pull connectors

---
+++ Specifications/Connectors/for_PullConnectors/when_registering.cs
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Dolittle.Logging;
+using Machine.Specifications;
+using Moq;
+using It = Machine.Specifications.It;
+
+namespace Dolittle.TimeSeries.Runtime.Connectors.for_PullConnectors
+{
+    public class when_registering
+    {
+        static ConnectorId connector_id;
+        static PullConnector pull_connector;
+        static PullConnectors pull_connectors;
+
+        Establish context = () =>
+        {
+            connector_id = Guid.NewGuid();
+            pull_connector = new PullConnector(connector_id, "Fourty Two", 42, new []
+            { 
+                (Tag)
+                "Fourty", (Tag)
+                "Two"
+            });
+            pull_connectors = new PullConnectors(Mock.Of<ILogger>());
+        };
+
+        Because of = () => pull_connectors.Register(pull_connector);
+
+        It should_consider_having_the_connector = () => pull_connectors.Has(connector_id).ShouldBeTrue();
+        It should_have_the_connector = () => pull_connectors.GetById(connector_id).ShouldEqual(pull_connector);
+    }
+}
---
+++ Specifications/Connectors/for_PullConnectors/when_unregistering.cs
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Dolittle.Logging;
+using Machine.Specifications;
+using Moq;
+using It = Machine.Specifications.It;
+
+namespace Dolittle.TimeSeries.Runtime.Connectors.for_PullConnectors
+{
+    public class when_unregistering
+    {
+        static ConnectorId connector_id;
+        static PullConnector pull_connector;
+        static PullConnectors pull_connectors;
+
+        Establish context = () =>
+        {
+            connector_id = Guid.NewGuid();
+            pull_connector = new PullConnector(connector_id, "Fourty Two", 42, new []
+            { 
+                (Tag)
+                "Fourty", (Tag)
+                "Two"
+            });
+            pull_connectors = new PullConnectors(Mock.Of<ILogger>());
+            pull_connectors.Register(pull_connector);
+        };
+
+        Because of = () => pull_connectors.Unregister(pull_connector);
+
+        It should_not_consider_having_the_connector = () => pull_connectors.Has(connector_id).ShouldBeFalse();
+    }    
+}
