6b9f1c971a307a17be13e8e4f4ec00582e2ac2ea
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-30 15:12:26 +0100

    Fixing typo

--- Specifications/Connectors/for_PushConnectors/when_registering.cs
+++ Specifications/Connectors/for_PushConnectors/when_registering.cs
@@ -20,7 +20,7 @@ namespace Dolittle.TimeSeries.Runtime.Connectors.for_PushConnectors
         {
             connector_id = Guid.NewGuid();
             push_connector = new PushConnector(connector_id, "Fourty Two");
-            push_connectors = new pushConnectors(Mock.Of<ILogger>());
+            push_connectors = new PushConnectors(Mock.Of<ILogger>());
         };
 
         Because of = () => push_connectors.Register(push_connector);
